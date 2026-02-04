9956ce739baadf47ba3a28369003864c025090f2
Dwaung <dwgardner96@gmail.com>
2026-02-04 05:34:36 +0000

    intial tests

---
+++ BusShuttleProject/BusShuttle.Tests/FilerSaverTests.cs
@@ -0,0 +1,26 @@
+﻿namespace BusShuttle.Tests;
+
+using BusShuttle;
+
+public class FileSaverTests
+{
+    FileSaver fileSaver;
+    string testFileName;
+
+    public FileSaverTests()
+    {
+        testFileName = "test-doc.txt";
+        fileSaver = new FileSaver(testFileName);
+    }
+
+    [Fact]
+    public void Test_FileSaver_Append()
+    {
+        // Act
+        fileSaver.AppendLine("Hello, World!");
+        
+        // Assert
+        var contentFromFile = File.ReadAllText(testFileName);
+        Assert.Equal("Hello, World!" + Environment.NewLine, contentFromFile);
+    }
+}
