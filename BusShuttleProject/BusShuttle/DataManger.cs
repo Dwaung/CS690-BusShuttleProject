c2c1e981741aac1bad52a725f27205c99412f62f
Dwaung <dwgardner96@gmail.com>
2026-02-04 09:01:39 +0000

    data manager introduced

---
+++ BusShuttleProject/BusShuttle/DataManger.cs
@@ -0,0 +1,44 @@
+namespace BusShuttle;
+
+public class DataManager
+{
+    FileSaver fileSaver;
+
+    public List<Loop> Loops { get; }
+    public List<Stop> Stops { get; }
+    public List<Driver> Drivers { get; }
+    public List<PassengerData> PassengerDataEntries { get; } = new List<PassengerData>();
+
+    public DataManager()
+    {
+        fileSaver = new FileSaver("passenger-data.txt");
+
+        Loops = new List<Loop>();
+        Loops.Add(new Loop("Red"));
+        Loops.Add(new Loop("Green"));
+        Loops.Add(new Loop("Blue"));
+
+        Stops = new List<Stop>();
+        Stops.Add(new Stop("Music"));
+        Stops.Add(new Stop("Tower"));
+        Stops.Add(new Stop("Oakwood"));
+        Stops.Add(new Stop("Anthony"));
+        Stops.Add(new Stop("Letterman"));
+
+        Loops[0].Stops.Add(Stops[0]);
+        Loops[0].Stops.Add(Stops[1]);
+        Loops[0].Stops.Add(Stops[2]);
+        Loops[0].Stops.Add(Stops[3]);
+        Loops[0].Stops.Add(Stops[4]);
+
+        Drivers = new List<Driver>();
+        Drivers.Add(new Driver("Luke Skywalker"));
+        Drivers.Add(new Driver("Han Solo"));
+    }
+
+    public void AddNewPassengerData(PassengerData data)
+    {
+        this.PassengerDataEntries.Add(data); 
+        this.fileSaver.AppendData(data);
+    }
+}
