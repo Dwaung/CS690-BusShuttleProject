00181e6a7ab0b3f0cb2def43c6cbfba545cf65ef
Dwaung <dwgardner96@gmail.com>
2026-02-05 06:23:12 +0000

    5th

--- BusShuttleProject/BusShuttle.Tests/DataManagerTests.cs
+++ BusShuttleProject/BusShuttle.Tests/DataManagerTests.cs
@@ -7,17 +7,14 @@ public class DataManagerTests {
     DataManager dataManager;
 
     public DataManagerTests() {
-        // Setup a controlled test environment
         File.WriteAllText("stops.txt", "One" + Environment.NewLine + "Two" + Environment.NewLine + "Three" + Environment.NewLine + "Four" + Environment.NewLine + "Five");
         dataManager = new DataManager();
     }
 
     [Fact]
     public void Test_AddStop() {
-        // Initial count based on setup file
         Assert.Equal(5, dataManager.Stops.Count);
 
-        // Verify count increases after adding a stop
         dataManager.AddStop(new Stop("new stop"));
         Assert.Equal(6, dataManager.Stops.Count);
     }
